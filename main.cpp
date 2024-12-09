5960cf901c84119ee68f322e3a09f90ed690a445
Bish0pdev <62082484+Bish0pdev@users.noreply.github.com>
2024-12-09 14:46:55 -0600

    Refactored Vertices

--- main.cpp
+++ main.cpp
@@ -5,20 +5,6 @@
 #include <random>
 #include <vector>
 
-struct Vertex {
-    sf::Vector2f position;
-    sf::Color color = sf::Color::White; // Default uncolored
-    int id; // Unique identifier
-};
-
-struct Edge {
-    int v1; // Vertex 1 ID
-    int v2; // Vertex 2 ID
-};
-
-void addEdge(const Vertex& v1, const Vertex& v2, std::vector<Edge>& edges) {
-    edges.push_back({v1.id, v2.id});
-}
 
 // Function to add a vertex at the mouse position
 void addVertex(const sf::Vector2f& mousePosition, const sf::Color& color,
@@ -34,8 +20,8 @@ void addVertex(const sf::Vector2f& mousePosition, const sf::Color& color,
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Test");
-    std::vector<Vertex> vertices;
-    std::vector<Edge> edges;
+    std::vector<sf::CircleShape> vertices;
+   // std::vector<Edge> edges;
 
     const float fixedTimeStep = 1.0f / 60.0f; // 60 updates per second
     float accumulator = 0.0f;                // Tracks unprocessed time
@@ -64,8 +50,11 @@ int main()
 
             if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
                 if (!isMousePressed) {
-                    //On click
-                    
+                    std::cout << "Mouse button pressed" << std::endl;
+                    sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+                    sf::Vector2f worldPosition(static_cast<float>(mousePosition.x), static_cast<float>(mousePosition.y));
+
+                    addVertex(worldPosition, sf::Color::Red, vertices);
                     isMousePressed = true;
                 }
             } else {
@@ -73,9 +62,7 @@ int main()
             }
 
             if(isMousePressed) {
-                std::cout << "Mouse button pressed" << std::endl;
-                sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
-                sf::Vector2f worldPosition(static_cast<float>(mousePosition.x), static_cast<float>(mousePosition.y));
+                
             }
             //----
             if (event.type == sf::Event::KeyReleased) //input
@@ -92,10 +79,7 @@ int main()
         //Draw Vertices
         for (const auto& vertex : vertices)
         {
-            sf::CircleShape dot(4.f); // Radius of the dot
-            dot.setFillColor(vertex.color); // Match the vertex color
-            dot.setPosition(vertex.position.x - dot.getRadius(), vertex.position.y - dot.getRadius()); // Center the dot
-            window.draw(dot);
+            window.draw(vertex);
         }
 
         //Fixed Time Set
